f2542386ba339f4b3e85b233499db317f3d05356
shakil-ahmed-billal <itzxhakil@gmail.com>
2024-09-19 02:10:29 +0600

    new cash out options add and degin done

--- Script/home.js
+++ Script/home.js
@@ -19,3 +19,24 @@ document.getElementById('add-amount').addEventListener('click', function(event){
         alert('Please Provide Your Valid Info')
     }
 })
+
+// cash out function add ..
+document.getElementById('cash-out-btn').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const cashPin = document.getElementById('cash-out-pin').value;
+    let cashOut = document.getElementById('cash-out-amount').value;
+
+    if(cashPin === '1234'){
+        const inputCashOut = parseFloat(cashOut);
+
+        let cashBalance = document.getElementById('account-blc').innerText;
+        const newCashBalance = parseFloat(cashBalance);
+        const accountBalance = newCashBalance - inputCashOut ;
+        document.getElementById('account-blc').innerText = accountBalance;
+
+    }
+    else{
+        alert('Please Provide Your Valid Info')
+    }
+})
